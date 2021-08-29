de6c080cecc8674510268d4c1c4a0025f0b08ef8
Josh Bressers <josh@bress.net>
2021-08-29 14:40:42 -0500

    Add the ability to specify a mapping file

--- esbulkstream/esbulkstream.py
+++ esbulkstream/esbulkstream.py
@@ -1,12 +1,13 @@
 
 import os
+import json
 import elasticsearch
 import elasticsearch.helpers
 from elasticsearch import Elasticsearch
 
 class Documents:
 
-    def __init__(self, index, update_frequency=1000):
+    def __init__(self, index, update_frequency=1000, mapping='mapping.json'):
 
         if 'ESURL' not in os.environ:
             es_url = "http://localhost:9200"
@@ -17,6 +18,16 @@ class Documents:
         self.index = index
         self.frequency = update_frequency
         self.docs = []
+
+        # First let's see if the index exists
+        if self.es.indices.exists(self.index) is False:
+            # We have to create it and add a mapping, but only if a
+            # mapping.json file exists
+            if os.path.exists(mapping):
+                fh = open(mapping)
+                mapping = json.load(fh)
+                self.es.indices.create(self.index, body=mapping)
+
 
     def add(self, document_data, document_id):
 
